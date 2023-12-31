3e922a978e1c725bee5f2fcdba493675790db66d
Lupitot <paul.piauger2@gmail.com>
2023-12-31 11:30:35 +0100

    ajustement pour le rafrechissement des page

--- Back_API/middlewares/history.js
+++ Back_API/middlewares/history.js
@@ -1,15 +1,15 @@
-// history.js
-const path = require('path');
-const express = require('express');
+// // history.js
+// const path = require('path');
+// const express = require('express');
 
-module.exports = function history(staticFolder) {
-  return (req, res, next) => {
-    if (req.path.includes('.')) {
-      // Si la requête est pour une ressource statique, passez au middleware suivant
-      next();
-    } else {
-      // Sinon, renvoyez l'index.html
-      res.sendFile(path.resolve(staticFolder, 'index.html'));
-    }
-  };
-};
+// module.exports = function history(staticFolder) {
+//   return (req, res, next) => {
+//     if (req.path.includes('.')) {
+//       // Si la requête est pour une ressource statique, passez au middleware suivant
+//       next();
+//     } else {
+//       // Sinon, renvoyez l'index.html
+//       res.sendFile(path.resolve(staticFolder, 'index.html'));
+//     }
+//   };
+// };
--- Back_API/server.js
+++ Back_API/server.js
@@ -2,11 +2,11 @@
 const http = require('http');
 const path = require('path');
 const app = require('./app');
-const history = require('./middlewares/history');
+// const history = require('./middlewares/history');
 
 const port = process.env.PORT || 3000; 
 
-app.use(history(path.resolve(__dirname, '../Mon_API_DandD/frontapi/dist')));
+// app.use(history(path.resolve(__dirname, '../Mon_API_DandD/frontapi/dist')));
 
 const server = http.createServer(app);
 
